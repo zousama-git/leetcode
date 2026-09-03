6fa8550b58874f59938c0a28274db845bfc2d09a
zousama <mbekezeli071@gmail.com>
2026-09-03 09:27:06 +0200

    [LeetPush] Add Solution.java for 1. Two Sum

---
+++ 001-two-sum/Solution.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        for (int i = 0; i < nums.length; i++){
+        }
+            for (int j = 0; j < nums.length; j++){
+                if ((nums[i]+nums[j]) == target){
+            }
+                    positions[0] = i;
+                }
+    }
+                    break;
+
+        int [] positions = new int[2];
+                    positions[1] = j;
+
+
+
+        return positions;
+}
