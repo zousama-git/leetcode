56117a8f9e869a1414b4afcbe5a22b596f910ae1
zousama <mbekezeli071@gmail.com>
2026-09-03 09:20:10 +0200

    [LeetPush] Add Solution_v5.java for 242. Valid Anagram

---
+++ 242-valid-anagram/Solution_v5.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+       boolean isAnagram;
+
+        char [] arrS = s.toCharArray();
+        char [] arrT = t.toCharArray();
+        
+        Arrays.sort(arrS);
+        Arrays.sort(arrT);
+ 
+       return Arrays.equals(arrS, arrT);
+    }
+}
