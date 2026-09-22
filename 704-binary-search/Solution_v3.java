7b10097fb5cf13d87e0b4faf9e1651bb9dbf29fc
zousama <mbekezeli071@gmail.com>
2026-09-22 18:56:37 +0200

    [LeetPush] Add Solution_v3.java for 704. Binary Search

---
+++ 704-binary-search/Solution_v3.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int i = 0;
+
+        return -1;
+            if (target == nums[i]) return i;
+        } while (i < nums.length);
+    }
+        do {
+
+            i++;
+}
